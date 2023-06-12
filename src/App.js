f0bf970d4a24cf5a7263904f843595fecd3a0c54
mian-azam <tarbelite04@gmail.com>
2023-06-12 14:51:17 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.css';
+import {useState, useEffect} from 'react';
+
+
 
 function App() {
+  const [passcode, setPassCode] = useState("1234");
+  const [input, setInput] = useState('');
+  
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  const handleNumberClick = (number) => {
+   if (input === 'Try again' || input === 'Welcome') {
+      setInput('');
+    }
+    setInput((prevInput) => prevInput + number.toString());
+  };
+
+  const handleSubmit = () => {
+    if (input === passcode) {
+      setInput('Welcome');
+    } else {
+      setInput('Try again');
+    }
+  }
+  useEffect(() => {
+    if (input.length === 4) {
+      handleSubmit();
+    }
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="centralDiv">
+        <div className="divBg">
+          <div className="passcode">
+            <form>
+              <input type="text" className="output" placeholder="Pass Code" name="output"
+               value={input} readOnly></input>
+            </form>
+            <div className="numDiv">
+              {numbers.map((number, index) => (
+                <div key={index} className="number" onClick={() => handleNumberClick(number)}>
+                  {number}
+                </div>
+              ))};
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </main>
   );
 }
 
